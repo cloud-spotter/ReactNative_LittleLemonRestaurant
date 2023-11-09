8a575ea506f14b8835c1501c501e050092535a4a
cloud-spotter <cloudspotter0@gmail.com>
2023-11-09 16:02:38 +0000

    MenuItems component added. Spacing not right between components yet. Comments need deleting once styling/spacing finalised.

--- App.js
+++ App.js
@@ -5,19 +5,32 @@ import { View } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './components/WelcomeScreen';
+import MenuItems from './components/MenuItems';
 
 export default function App() {
   return (
     <>
       <View style={{flex: 1, backgroundColor: '#495E57',}}>
         <LittleLemonHeader />
-      </View>
-      <View style={{flex: 4, backgroundColor: '#495E57', justifyContent: 'flex-start'}}>
         <WelcomeScreen />
-      </View>
-      <View style={{flex: 1, backgroundColor: '#495E57',}}>
+        <MenuItems />
         <LittleLemonFooter />
       </View>
     </>
   );
 }
+
+// return (
+//   <>
+//     <View style={{flex: 1, backgroundColor: '#495E57',}}>
+//       <LittleLemonHeader />
+//     </View>
+//     <View style={{flex: 4, backgroundColor: '#495E57', justifyContent: 'flex-start'}}>
+//       <WelcomeScreen />
+//     </View>
+//     <View style={{flex: 1, backgroundColor: '#495E57',}}>
+//       <LittleLemonFooter />
+//     </View>
+//   </>
+// );
+// }
